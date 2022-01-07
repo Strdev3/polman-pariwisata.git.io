31fdfe49e8eb2e2f33b3ca3518d11aae570052fb
Strdev3 <93799768+Strdev3@users.noreply.github.com>
2022-01-07 09:09:22 +0700

    Add files via upload

---
+++ website/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $("#AutoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
+});
+
+$(document).ready(function () {
+  $("#autowidth").lightSlider({
+    autowidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
+});
